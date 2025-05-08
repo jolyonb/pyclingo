bd5c133b67020b0b7de460aef9a372736b1e4358
Jolyon Bloomfield <jkb84@cornell.edu>
2025-05-08 09:11:15 -0400

    Refactoring out helper functions

---
+++ pyclingo/utils.py
@@ -0,0 +1,52 @@
+from typing import Sequence
+
+from pyclingo import Variable
+from pyclingo.term import Term
+
+
+def collect_variables(*terms: Term | Sequence[Term] | None) -> set[Variable]:
+    """
+    Given a list of terms, collect all variables used in them.
+
+    Accepts terms of the following types:
+    * Term
+    * List[Term]
+    * None
+
+    Returns:
+        set[Variable]: Set of variables
+    """
+    used_variables: set[Variable] = set()
+
+    for term in terms:
+        if term is None:
+            continue
+
+        elif isinstance(term, list):
+            for t in term:
+                used_variables.update(t.collect_variables())
+
+        elif isinstance(term, Term):
+            used_variables.update(term.collect_variables())
+
+        else:
+            raise ValueError(f"Bad term type: {term}")
+
+    return used_variables
+
+
+def create_unique_variable_name(used_variables: set[Variable], preferred_names: list[str]) -> Variable:
+    """
+    Given a set of used variables and a list of preferred names, construct a unique variable name.
+    Uses the first available name in the preferred names list, or starts appending numbers to the last one if needed.
+    """
+    for name in preferred_names:
+        if all(var.name != name for var in used_variables):
+            return Variable(name)
+
+    # If all preferred names are taken, use numeric suffix with the last preferred name
+    base_name = preferred_names[-1]
+    counter = 1
+    while any(var.name == f"{base_name}{counter}" for var in used_variables):
+        counter += 1
+    return Variable(f"{base_name}{counter}")
